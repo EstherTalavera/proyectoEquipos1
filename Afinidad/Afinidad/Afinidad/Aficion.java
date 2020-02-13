d1079e61b1502195870acc7acf8e1995edf1bf4e
Esther <esthertalaveraclase@gmail.com>
2020-02-13 12:52:49 +0100

    La primera version
    Pronto empezaremos a actualizar
    Por ahora solo estan declaradas las variables necesarias
    
    Signed-off-by: Esther <esthertalaveraclase@gmail.com>

---
+++ Afinidad/Afinidad/Afinidad/Aficion.java
@@ -0,0 +1,19 @@
+package Afinidad;
+
+public class Aficion {
+	
+	/**
+	 * Atributo cod_Aficion
+	 * 
+	 * Indica el numero dado a x aficion
+	 */
+	private int cod_Aficion;
+	
+	/**
+	 * Atributo aficiones
+	 * 
+	 * AIndica el nombre de la aficion
+	 */
+	private String nombre_Aficion;
+}
+
---
+++ Afinidad/Afinidad/Afinidad/Persona.java
@@ -0,0 +1,34 @@
+package Afinidad;
+
+/**
+ * <h2>Clase Persona <h2>
+ * Esta clase contiene los datos de una persona necesarios para el programa
+ * 
+ * @author Grupo3
+ * @version 1.0
+ *
+ */
+public class Persona {
+	
+	/**
+	 * Atributo nombre
+	 * 
+	 * Indica el nombre de la persona
+	 */
+	private String nombre;
+	
+	/**
+	 * Atributo edad
+	 * 
+	 * Indica el edad de la persona
+	 */
+	private int edad;
+	
+	/**
+	 * Atributo aficiones
+	 * 
+	 * Array que contiene las aficiones de la persona
+	 */
+	private Aficion [] aficiones;
+}
+
